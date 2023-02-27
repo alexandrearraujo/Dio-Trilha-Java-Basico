8b436d264d76caa39c73d7eb7cebddc72ba6a3d1
alexandrearraujo <alexandreplay12@hotmail.com>
2023-02-27 19:38:41 -0300

    exemplo data de nasciemnto

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,16 @@
+public class operadores {
+    
+    public static void main(String[] args) {
+        int idade;
+        short ano;
+
+        idade = 27;
+        ano = 2023;
+
+        short anoNascmento = (short) (idade - ano);
+    
+   System.out.println(anoNascmento);
+    }
+        
+    
+}
